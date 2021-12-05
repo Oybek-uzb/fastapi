5bb773b55972881e379d4c46c8c91fb1fe5b6255
Oybek-uzb <programmingiswonderful@gmail.com>
2021-12-05 11:19:27 +0500

    delete method added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 from uuid import UUID, uuid4
 from fastapi import FastAPI
+from pydantic.types import UUID1
 
 from models import Gender, Role, User
 
@@ -23,6 +24,13 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(id: UUID):
+    for user in db:
+        if user.id == id:
+            db.remove(user)
+            return
 
 
 # localhost:8000/docs -> automatic genereted swagger
