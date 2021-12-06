d3aa0a0c8f7be5f430f50c89142eac8248907342
Oybek-uzb <programmingiswonderful@gmail.com>
2021-12-06 14:44:34 +0500

    raising excetions learned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from uuid import UUID, uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic.types import UUID1
 
 from models import Gender, Role, User
@@ -26,11 +26,16 @@ async def register_user(user: User):
     return {"id": user.id}
 
 @app.delete("/api/v1/users/{user_id}")
-async def delete_user(id: UUID):
+async def delete_user(user_id: UUID):
+    print(user_id)
     for user in db:
-        if user.id == id:
+        if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
 
 
 # localhost:8000/docs -> automatic genereted swagger
