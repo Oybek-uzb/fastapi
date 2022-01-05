d17102f90792ec2c3aeb17e5a177cb9028ddf64c
Oybek-uzb <programmingiswonderful@gmail.com>
2022-01-05 21:43:16 +0500

    coding main

--- sql_app/__init__.py
+++ sql_app/__init__.py
@@ -1,1 +1,8 @@
 # __init__.py is just an empty file, but it tells Python that sql_app with all its modules (Python files) is a package.
+# SQLAlchemy is the Python SQL toolkit and Object Relational Mapper
+# In these examples we will use SQLite, but it is not difficult adapt to any others databases supported by SQLAlchemy
+# They are: PostgreSQL, MySQL, SQLite, Oracle, Microsoft SQL Server, etc.
+# An ORM has tools to convert ("map") between objects in code and database tables ("relations")
+# With an ORM, you normally create a class that represents a table in a SQL database,
+# each attribute of the class represents a column, with a name and type.
+# These ORMs also have tools to make the connections or relations between tables or entities.
